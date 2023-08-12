bbb9e1b65242ed024538d060a40514dad63561db
mbmande <mbsmande@gmail.com>
2023-08-12 16:44:25 -0700

    my 1-last_digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,9 @@ int main(void)
 		if (o > 5)
 			printf("Last digit of %d is %d and is greater than 5", n, o);
 		else if (o == 0)
-			printf("last digit of %d is %d and is 0", n, o);
-		else if (o < 6 && o !=0)
-			printf("last digit of %d is %d and is less than 6 and not 0", n, o);
+			printf("Last digit of %d is %d and is 0", n, o);
+		else if (o < 6 && o != 0)
+			printf("Last digit of %d is %d and is less than 6 and not 0", n, o);
 		printf("\n");
 		return (0);
 }
