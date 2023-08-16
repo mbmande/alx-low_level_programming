3de7a80c9dc2e79871f80461b5ede279c43587af
mbmande <mbsmande@gmail.com>
2023-08-16 03:49:20 -0700

    my 6-abs.c commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - it computes the absolute value of an integer
+ * @a: fumction parameter
+ * Return: a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
