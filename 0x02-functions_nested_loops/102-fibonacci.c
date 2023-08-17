73cfddbcad54e50e28b6dd28067fb81ab7d8a614
mbmande <mbsmande@gmail.com>
2023-08-17 07:30:50 -0700

    my 102-fibonacci.c commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main prints fibonacci numbers
+ * Return: void
+ */
+
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int next;
+	int i;
+
+	printf("%lu, ", num1);
+	for (i = 0; i < 50; i++)
+	{
+		printf("%lu", num2);
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
