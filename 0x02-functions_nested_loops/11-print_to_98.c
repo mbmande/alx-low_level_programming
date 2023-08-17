e2bd27502ac13a48c877dbbc545397e41574baec
mbmande <mbsmande@gmail.com>
2023-08-17 03:33:59 -0700

    my 11-print_to_98.c commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints from any number to 98
+ * @n: number to start with
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if  (n >= 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n > 0)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else if (n == 98)
+		printf("%d\n", n);
+}
