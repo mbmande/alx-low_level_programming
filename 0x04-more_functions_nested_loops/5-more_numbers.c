a0582b62dca3f0ac38abe0bf7928b23bf8c77caf
mbmande <mbsmande@gmail.com>
2023-08-18 05:03:21 -0700

    my 5-more_numbers commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * more_numbers - it's for printing numbers from 0 to 14, 10 times
+ * Return: 0 always.
+ */
+
 void more_numbers(void)
 {
 	int i, j;
@@ -9,7 +14,7 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
-				_putchar(j / 10 +'0');
+				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
