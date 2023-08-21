2c854db5bb2c312cdc484ec7127fad7cc04907ee
mbmande <mbsmande@gmail.com>
2023-08-21 09:20:50 -0700

    my 0-98 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset n to 98
+ * @n: pointer to the value changed
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my pointer, array and string readme file
