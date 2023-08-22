fdab250efdb27ea060eaf188229920254e44aa2b
mbmande <mbsmande@gmail.com>
2023-08-22 09:11:43 -0700

    my ./8-print_array commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (a[i] != -1024)
+			printf(", ");
+	}
+	printf("\n");
+}
