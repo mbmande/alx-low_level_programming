98958aae98c842ee044f2b64fac997fe01ebea0d
mbmande <mbsmande@gmail.com>
2023-08-22 09:17:46 -0700

    my 8-print_array.c commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - it prints the n elements of an array
+ * @a: array to be used
+ * @n: number of elements to be printed
+ * Return: void
+ */
+
 void print_array(int *a, int n)
 {
 	int i;
