96f40ff18f794d12d4fc89ff22a53f5eea3f71c5
mbmande <mbsmande@gmail.com>
2023-08-24 00:51:46 -0700

    my 4-rev_array commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - it reverse the number of array in a
+ * @a: array name
+ * @n: numbers of array a
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		j = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = j;
+	}
+}
