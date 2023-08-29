5dfc38d3c32d8b28d07f70cdcc5f23feb5550622
mbmande <mbsmande@gmail.com>
2023-08-29 06:02:34 -0700

    my 2-strchr commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MIAN_H
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
