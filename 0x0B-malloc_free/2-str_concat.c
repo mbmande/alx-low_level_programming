559af9c02007c9fafa0463e39b084809951c0876
mbmande <mbsmande@gmail.com>
2023-09-06 13:18:32 -0700

    my 2-str_concat.c commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (output == NULL)
 		return (NULL);
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		output[i] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
 		output[s1len + 1] = s2[i];
