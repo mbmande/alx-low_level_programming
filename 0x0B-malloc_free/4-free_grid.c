1780278fb8ce5752e2a4e80cfc5fa0affde35b7f
mbmande <mbsmande@gmail.com>
2023-09-07 15:18:20 -0700

    my valgrind ./f commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - free memory previously allocated
+ * @grid: pointer to teh array
+ * @height: number of rows
+ */
+
+void free_grid(int **grid, int height)
+{
+
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
