ca3965cdd9dce616f8c1ab90d6f6a0f641bd82c3
mbmande <mbsmande@gmail.com>
2023-09-12 17:13:52 -0700

    my dog.h commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age dog
+ * @owner: onwer of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
