9fdc9f8719af6035cf4e438a3af6c0dbb7794bb7
mbmande <mbsmande@gmail.com>
2023-09-14 15:38:02 -0700

    my print_stings commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,6 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *x = va_arg(arg, char *);
+
 		if (x == NULL)
 			printf("(nil)");
 		else
