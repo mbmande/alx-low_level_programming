c0326b025ef2b4c85c37744d7dac3c9ef924eb06
mbmande <mbsmande@gmail.com>
2023-09-26 04:03:27 -0700

    end node commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 
+/**
+ * add_nodeint_end - it adds node the end
+ * @n: numbers in the node
+ * @head: head of the linked list
+ * Return: the address of the head
+ */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	int num = n; 
+	int num = n;
 
 	listint_t *newnode, *tmp;
-	
+
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
-	
+
 	newnode->n = num;
 	newnode->next = NULL;
 	tmp = *head;
