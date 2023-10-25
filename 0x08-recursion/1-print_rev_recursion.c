102a6f737482c9b97906660e71617b8576f3771d
mbmande <mbsmande@gmail.com>
2023-10-25 11:32:51 +0100

    my rev commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - it print in reverse
+ * @s: characters to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
