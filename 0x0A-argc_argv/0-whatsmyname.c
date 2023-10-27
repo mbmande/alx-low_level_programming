4992eefdd076094736e8c5e9e3f8a4465d4e7f22
mbmande <mbsmande@gmail.com>
2023-10-27 16:09:34 -0700

    my argv commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - it returns the file name
+ * @argc: number of argument
+ * @argv: argument holder
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
