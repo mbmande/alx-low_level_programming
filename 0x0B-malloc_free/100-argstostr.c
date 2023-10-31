ef6fcbe6e056521bb1e2ec3eb6cd8af35f02ece1
mbmande <mbsmande@gmail.com>
2023-10-31 09:53:57 -0700

    my agrs commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * argstostr -  args concat
+ * @ac: first parameter
+ * @av: second parameter
+ * Return: out
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, cunt = 0, k = 0;
+	char *out;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			cunt++;
+		cunt++;
+	}
+	out = malloc(sizeof(char) * cunt + 1);
+	if (out == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			out[k] = av[i][j];
+			k++;
+		}
+		out[k++] = '\n';
+	}
+	out[k] = '\0';
+	return (out);
+}
