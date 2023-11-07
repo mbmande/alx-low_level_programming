0aae1decade3247ed67919044fce97230ed9419f
mbmande <mbsmande@gmail.com>
2023-11-07 03:04:44 +0100

    my free comit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free
+ * @d: free dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
