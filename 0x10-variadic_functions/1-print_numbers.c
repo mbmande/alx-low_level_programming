d96db9b01decfa165204a13ecbc6b21056355c43
mbmande <mbsmande@gmail.com>
2023-11-09 06:41:45 -0800

    my print nums commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,10 +7,11 @@
  * @n: args nums
  */
 
-void print_numbers(const char * separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
