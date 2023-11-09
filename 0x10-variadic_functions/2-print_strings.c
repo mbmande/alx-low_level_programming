c0cfc45267d0add7378b24b01d413e5390a8ba5b
mbmande <mbsmande@gmail.com>
2023-11-09 17:19:05 +0100

    my string printing commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - it prints strs
+ * @separator: in between
+ * @n: nums
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *x = va_arg(args, char *);
+		if (x == NULL)
+			printf("(nil)");
+		else
+			printf("%s", x);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
