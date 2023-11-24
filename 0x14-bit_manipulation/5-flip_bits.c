8701126c5c770ef8e6af71ae30467d7aafba9ff5
mbmande <mbsmande@gmail.com>
2023-11-24 09:13:40 +0100

    fliping bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - the numbers of bits
+ * @n: parameter 1
+ * @m: parameter 2
+ * Return: number
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int o;
+	int cunt = 0;
+	unsigned long int now; 
+	unsigned long int later = n ^ m;
+
+	for (o = 63; o >= 0; o--)
+	{
+		now = later >> o;
+		if (now & 1)
+			cunt++;
+	}
+	return (cunt);
+}
