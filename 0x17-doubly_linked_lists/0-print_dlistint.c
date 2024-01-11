cc32a91ecbff5ec49950913a9db3259f67a431a7
mbmande <mbsmande@gmail.com>
2024-01-11 15:06:42 -0800

    all listed

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - it prints everything
+ * @h: the head
+ * Return: node
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nd = 0;
+	while (h)
+	{
+		nd++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nd);
+}
