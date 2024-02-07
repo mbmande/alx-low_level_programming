aa43b5ad7d6fbb7c92eaf34ad74689512306f361
mbmande <mbsmande@gmail.com>
2024-02-07 02:09:58 -0800

    my 2 files commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * ket_index - gets the key index
+ * key_index - gets the key index
  * @key: the key
  * @size: the size
  * Return: the index of the key
  * Description: algorithm
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size); /* the returning*/
