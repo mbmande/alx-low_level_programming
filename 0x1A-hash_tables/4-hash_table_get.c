c36469796cb47f64dd9bf92560d0a62011663bdd
mbmande <mbsmande@gmail.com>
2024-02-07 15:09:39 -0800

    get hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,21 +1,27 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_get - ======
+ * @ht: ======
+ * @key: =====
+ *
+ * Return: ======
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int dex;
-    hash_node_t *nd;
+	unsigned long int dex;
+	hash_node_t *nd;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-    dex = key_index((const unsigned char *)key, ht->size);
+
+	dex = key_index((const unsigned char *)key, ht->size);
 	if (dex >= ht->size)
 		return (NULL);
 
 	nd = ht->array[dex];
 	while (nd && strcmp(nd->key, key) != 0)
 		nd = nd->next;
-        
-    return ((nd == NULL) ? NULL : nd->value);
+	return ((nd == NULL) ? NULL : nd->value);
 }
