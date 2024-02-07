4a769c4db3c14ad2bf455e86c060f5af92b61367
mbmande <mbsmande@gmail.com>
2024-02-07 13:14:56 -0800

    hash set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,12 +17,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	cpy_value = strdup(value);
 	if (cpy_value == NULL)
 		return (0);
-
-
 	dex = key_index((const unsigned char *)key, ht->size);
 
 	for (o = dex; ht->array[o]; o++)
@@ -34,26 +31,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-
 	sabo = malloc(sizeof(hash_node_t));
-	
 	if (sabo == NULL)
 	{
 		free(cpy_value);
 		return (0);
 	}
-
 	sabo->key = strdup(key);
 	if (sabo->key == NULL)
 	{
 		free(sabo);
 		return (0);
 	}
-
 	sabo->value = cpy_value;
 	sabo->next = ht->array[dex];
 	ht->array[dex] = sabo;
-
-
 	return (1);
 }
